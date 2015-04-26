6196551b6d011c425d379d6778ee0ca61f909b28
allan macdonald <iggymacd@gmail.com>
2015-04-26 10:55:31 -0300

    second

--- game.go
+++ game.go
@@ -4,13 +4,31 @@ import "fmt"
 
 // item represents a token or text string returned from the scanner.
 type action struct {
-	typ itemType // The type of this item.
+	typ actionType // The type of this item.
 	//pos Pos      // The starting position, in bytes, of this item in the input string.
 	val string // The value of this item.
 }
 
-func main() {
+// stateFn represents the state of the game as a function that returns the next state.
+type stateFunc func(*game) stateFunc
 
+// lexer holds the state of the scanner.
+type game struct {
+	name  string // the name of the input; used only for error reports
+	input string // the string being scanned
+	//leftDelim  string    // start of action
+	//rightDelim string    // end of action
+	state stateFunc // the next action function to enter
+	//pos        Pos       // current position in the input
+	//start      Pos       // start position of this action
+	//width      Pos       // width of last rune read from input
+	//lastPos    Pos       // position of most recent action returned by nextItem
+	actions chan action // channel of scanned actions
+	//parenDepth int         // nesting depth of ( ) exprs
+}
+
+func main() {
+	fmt.Println("here...")
 }
 
 func (i action) String() string {
@@ -27,16 +45,16 @@ func (i action) String() string {
 	return fmt.Sprintf("%q", i.val)
 }
 
-// itemType identifies the type of lex items.
-type itemType int
+// actionType identifies the type of lex items.
+type actionType int
 
 const (
-	itemError        itemType = iota // error occurred; value is text of error
-	itemBool                         // boolean constant
-	itemChar                         // printable ASCII character; grab bag for comma etc.
-	itemCharConstant                 // character constant
-	itemComplex                      // complex constant (1+2i); imaginary is just a number
-	itemColonEquals                  // colon-equals (':=') introducing a declaration
+	itemError        actionType = iota // error occurred; value is text of error
+	itemBool                           // boolean constant
+	itemChar                           // printable ASCII character; grab bag for comma etc.
+	itemCharConstant                   // character constant
+	itemComplex                        // complex constant (1+2i); imaginary is just a number
+	itemColonEquals                    // colon-equals (':=') introducing a declaration
 	itemEOF
 	itemField      // alphanumeric identifier starting with '.'
 	itemIdentifier // alphanumeric identifier not starting with '.'
